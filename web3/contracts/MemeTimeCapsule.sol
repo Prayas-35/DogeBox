9983229729c72836ca915f314a54e515b8ff11b2
Prayas <prayaspal04@gmail.com>
2024-10-30 22:19:05 +0530

    contract

---
+++ web3/contracts/MemeTimeCapsule.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MemeTimeCapsule is ERC721URIStorage {
+    struct Meme {
+        uint256 id;
+        string ipfsHash;
+        uint256 unlockTime;
+    }
+
+    mapping(uint256 => Meme) public memes;
+    uint256[] public memeIds;
+
+    uint256 private _nextTokenId = 0;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC721("MemeTimeCapsule", "MTC") {}
+
+    function mintMeme(
+        address to,
+        string memory ipfsHash,
+        uint256 unlockTime
+    ) public returns (uint256) {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, ipfsHash);
+        memeIds.push(tokenId);
+        memes[tokenId] = Meme(tokenId, ipfsHash, unlockTime);
+
+        return tokenId;
+    }
+
+    /*Getter Functions */
+
+    // Function to get all unlocked memes as an array of Meme structs
+    function getUnlockedMemes() public view returns (Meme[] memory) {
+        uint256 unlockedCount = 0;
+        uint256 memeCount = memeIds.length;
+
+        // First, count the number of unlocked memes to initialize the array
+        for (uint256 i = 0; i < memeCount; i++) {
+            if (block.timestamp >= memes[i].unlockTime) {
+                unlockedCount++;
+            }
+        }
+
+        // Create an array to store unlocked Meme structs
+        Meme[] memory unlockedMemes = new Meme[](unlockedCount);
+        uint256 index = 0;
+
+        // Populate the unlocked memes array
+        for (uint256 i = 0; i < memeCount; i++) {
+            if (block.timestamp >= memes[i].unlockTime) {
+                unlockedMemes[index] = memes[i];
+                index++;
+            }
+        }
+
+        return unlockedMemes;
+    }
+}
